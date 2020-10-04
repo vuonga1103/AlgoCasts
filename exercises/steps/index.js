3b92b5e79b49e97223e0ae8733abb70e86715afb
vuonga1103 <vuonga1103@yahoo.com>
2020-10-04 14:13:00 -0400

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,63 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+
+  // *** SOLUTION 1 ***
+  // create an outer loop that will print each row until the number of rows reaches 2 (n)
+  // create an inner loop that will add #'s and spaces to a row
+
+  // the first row (idx 0) will have 1 # (idx + 1), and the remainder are spaces, until the length reaches 2 (n)
+  // the second row (idx 1) will have 2 # (idx + 2), and the remainder are spaces, until the length reaches 2 (n)
+
+  // let rowCount = 0
+
+  // while (rowCount < n) {
+  //   rowCount += 1
+  //   let row = '';
+
+  //   while (row.length < rowCount) row += '#'
+  //   while (row.length < n) row += ' '
+  //   console.log(row)
+  // }
+
+  // *** SOLUTION 2 ***
+  // for (let r = 0; r < n; r++) {
+  //   let row = '';
+
+  //   for (let h = 0; h <= r; h++) {
+  //     row += "#"
+  //   }
+
+  //   for (let s = r + 1; s < n; s++) {
+  //     row += ' '
+  //   }
+
+  //   console.log(row)
+  // }
+// }
+
+
+// *** SOLUTION 3 - RECURSION ***
+
+function steps(n, spaces = 0) {
+
+  if (spaces === n) return;
+
+  let result = '';
+  let spacesAdded = 0;
+
+  while (spacesAdded < spaces) {
+    result += ' ';
+    spacesAdded++
+  } 
+  
+  while (result.length < n) result = '#' + result;
+  
+  steps(n, spaces + 1)
+
+  console.log(result)
+}
+
 
 module.exports = steps;
