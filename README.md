1514dd269f94dbd7f05d37e696e6982c093b23ae
vuonga1103 <vuonga1103@yahoo.com>
2020-10-07 16:36:34 -0400

    matrix

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # AlgoCasts
 
 Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
+
+Example of running test--
+cd into /exercises, run `jest fib/test.js --watch`
+
+To use debugger--
+
+1. call the function
+2. cd in to the folder
+3. run `node inspect index.js`
+4. use `c` to continue
+5. use `repl` to examine variables
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,125 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+
+  // Create an array with n number of subarrays, within each subarray is n number of elements, initially set to empty string
+  let result = []
+
+  for (let subArrIdx = 0; subArrIdx < n; subArrIdx++) {
+    let subArr = [];
+    for (let i = 0; i < n; i ++) subArr.push('')
+    result.push(subArr)
+  }
+
+  let currentPosition = [0, 0];
+  let currentNum = 1
+
+  // Create helper functions that takes in a rowIdx, and a columnIdx, and fill up the result array in a particular direction starting at that position
+
+  const fillRight = ([rowIdx, columnIdx]) => {
+    let row = result[rowIdx];
+    let startingColumnIdx = result[rowIdx][columnIdx] ? columnIdx + 1 : columnIdx
+    let num = currentNum
+    let endingColumnIdx;
+
+    for (let i = startingColumnIdx; i < row.length; i++) {
+      if (result[rowIdx][i]) break;
+
+      endingColumnIdx = i;
+      result[rowIdx][i] = num; 
+      num ++;
+    }
+
+    currentPosition = [rowIdx, endingColumnIdx]
+    currentNum = num
+  }
+
+  const fillDown = ([rowIdx, columnIdx]) => {
+    let startingRowIdx = rowIdx + 1;
+    let num = currentNum;
+    let endingRowIdx;
+
+    // Loop through each row of result, starting at startingRowIdx
+    for (let i = startingRowIdx; i < result.length; i++) {
+      
+      if (result[i][columnIdx]) break;
+
+      endingRowIdx = i;
+      result[i][columnIdx] = num;
+      num ++
+    }
+
+    currentPosition = [endingRowIdx, columnIdx]
+    currentNum = num;
+  }
+
+  const fillLeft = ([rowIdx, columnIdx]) => {
+    let row = result[rowIdx];
+    let startingColumnIdx = columnIdx - 1;
+    let num = currentNum;
+    let endingColumnIdx;
+
+    for (let i = startingColumnIdx; i >= 0; i--) {
+      
+      if (result[rowIdx][i]) break;
+
+      endingColumnIdx = i;
+      result[rowIdx][i] = num;
+      num++;
+    }
+
+    currentPosition = [rowIdx, endingColumnIdx]
+    currentNum = num
+  }
+
+  const fillUp = ([rowIdx, columnIdx]) => {
+    let startingRowIdx = rowIdx - 1;
+    let num = currentNum;
+    let endingRowIdx;
+
+    for (let i = startingRowIdx; i >= 0; i--) {
+      if (result[i][columnIdx]) break;
+
+      endingRowIdx = i;
+      result[i][columnIdx] = num;
+      num ++
+    }
+
+    currentPosition = [endingRowIdx, columnIdx]
+    currentNum = num;
+  }
+
+  // // Create an array to store helper functions
+  let helperFunctions = [fillRight, fillDown, fillLeft, fillUp]
+
+  function resultIsFilled() {
+    for (let subArrIdx = 0; subArrIdx < result.length; subArrIdx++) {
+      const subArr = result[subArrIdx]
+      for (let i = 0; i < subArr.length; i++) {
+        const elem = subArr[i];
+        if (!elem) return false;
+      }
+    }
+    return true;
+  }
+
+  let x = 0;
+
+  while (!resultIsFilled()) {
+    helperFunctions[x](currentPosition)
+    if (x === 3) {
+      x = 0
+    } else {
+      x++ 
+    }
+  }
+
+  return result 
+}
+
+
+
+
 
 module.exports = matrix;
